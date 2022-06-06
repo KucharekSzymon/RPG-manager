fe68cf0a8fdf3c232189b55fdbb20f251aa98f41
Szymekk <skucharek2@gmail.com>
2022-06-06 20:12:13 +0200

    links w/o reloading

--- src/App.vue
+++ src/App.vue
@@ -3,33 +3,12 @@ import HelloWorld from "./components/HelloWorld.vue";
 import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
-<!-- <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template> -->
 <template>
-  <img
-    alt="Vue logo"
-    class="logo"
-    src="./assets/logo.svg"
-    width="125"
-    height="125"
-  />
+  <img alt="Vue logo" src="./assets/logo.svg" />
+  <br />
+
+  <router-link to="/">Home</router-link>
+  <router-link to="/about">About</router-link>
   <router-view></router-view>
 </template>
 
