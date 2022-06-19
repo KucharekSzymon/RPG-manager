7e8dd1f6d41d47b6507904b2bd188a5a53c25c3a
Daniel <danielopyd99@gmail.com>
2022-06-19 17:39:22 +0200

    edit for place

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,14 +3,14 @@ import Home from "@/views/Home-page.vue";
 import About from "@/views/About-page.vue";
 import Login from "@/views/Login-page.vue";
 import Create from "@/components/posts/Create.vue";
-
+import Edit from "@/components/posts/Edit.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/about", name: "About", component: About },
   { path: "/login", name: "Login", component: Login },
   { path: "/create", name: "Create", component: Create },
-
+  { path: "/edit/:id", name: "Edit", component: Edit },
 ];
 
 const router = createRouter({
